248251294a6eb08a4d836596fe7ff3fdcf60c8d9
Charles-LM <clewismangali@gmail.com>
2021-10-08 00:34:11 +1100

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(msg, num) {
+    if (num < 0) return 'ERROR'
+    let string = ''
+    for (let i = 0; i < num; i++) {
+      string += msg
+    }
+    return string;
+  };
+
+
+
+//finished
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
