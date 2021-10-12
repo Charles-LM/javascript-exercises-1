a3ee7ad8d6875057871d5f14a33d9496d65ccb63
Charles-LM <clewismangali@gmail.com>
2021-10-12 22:07:57 +1100

    leapYears done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0){
+        
+        return "ERROR";
+    }
 
+    if(min > max){
+        const hold = min;
+        min = max;
+        max = hold;
+    }
+
+    for(i = min; i < max + 1; i++){
+        sum += i;
+
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(leapTest) {
+    let leapYear = false;
+    if(leapTest % 4 === 0){
+        leapYear = true;
+        if(leapTest % 100 === 0){
+            leapYear = false;
+            if(leapTest % 400 === 0){
+                leapYear = true;
+            }
+        }
+    }
+    return leapYear;
+
+    //return leapTest % 4 === 0 && (leapTest % 100 != 0 || leapTest % 400 === 0)
+
+
+
+  
+    
 
 };
 
